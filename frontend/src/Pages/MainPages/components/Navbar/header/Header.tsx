0a9677db04c9c38fb3e3c73c67b40e0b67792855
Moustafa <moustafaadel097@gmail.com>
2024-05-30 02:15:24 +0300

    header

--- frontend/src/Pages/MainPages/components/Navbar/header/Header.tsx
+++ frontend/src/Pages/MainPages/components/Navbar/header/Header.tsx
@@ -210,7 +210,7 @@ export default function Header() {
           <WhiteBtnMenu
             name="book a table"
             class="hover:bg-[#DBDFD04e]"
-            state={userState.state}
+            state={userState?.state}
           />
         </div>
       </div>
